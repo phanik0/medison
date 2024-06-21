383ed2e9ddb0c4fac4e2b48a1d052db36fa9ec42
phanik0 <gacigogi3000@naver.com>
2024-06-21 10:44:50 +0900

    CreateProject

---
+++ src/test/java/com/medison/MedisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedisonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
