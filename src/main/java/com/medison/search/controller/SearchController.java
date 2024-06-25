a8f7140433c4cf7ba855eeeccab007265d36637f
Lr2l1 <hoit5it@naver.com>
2024-06-25 11:25:48 +0900

    Create Search 페이징처리, 정보불러오기

--- src/main/java/com/medison/search/controller/SearchController.java
+++ src/main/java/com/medison/search/controller/SearchController.java
@@ -2,41 +2,52 @@ package com.medison.search.controller;
 
 import com.medison.detail.domain.Study;
 import com.medison.search.service.SearchService;
+import com.medison.patient.domain.Patient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @Controller
 public class SearchController {
+
     private final SearchService searchService;
 
-    // 모든 스터디 목록을 가져와 HTML 페이지를 반환하는 메소드
     @GetMapping("/main")
-    public String getAllStudies(Model model, @RequestParam(defaultValue = "0") int page) {
+    public String getAllStudies(Model model,
+                                @RequestParam(defaultValue = "0") int page,
+                                @RequestParam(required = false) String patientCode,
+                                @RequestParam(required = false) String patientName,
+                                @RequestParam(required = false) String modality,
+                                @RequestParam(required = false) Long reportStatus,
+                                @RequestParam(required = false) Long examStatus) {
+
         Pageable pageable = PageRequest.of(page, 5);
-        Page<Study> studyPage = searchService.findAllWithPage(pageable);
+        Page<Study> studyPage = searchService.searchStudies(patientCode, patientName, modality, reportStatus, examStatus, pageable);
+
         model.addAttribute("studies", studyPage.getContent());
         model.addAttribute("currentPage", page);
         model.addAttribute("totalPages", studyPage.getTotalPages());
-        return "main"; // src/main/resources/templates/main.html 파일을 렌더링
+
+        if (patientCode != null && !patientCode.isEmpty()) {
+            Patient patient = searchService.getPatientByCode(Integer.parseInt(patientCode));
+            if (patient != null) {
+                model.addAttribute("patient", patient);
+            }
+        }
+
+        return "main"; // src/main/webapp/WEB-INF/jsp/main.jsp 파일을 렌더링
     }
 
-    // 특정 페이지의 스터디 목록을 가져와 HTML 페이지를 반환하는 메소드
-    @GetMapping("/main/{page}")
-    public String getAllStudies(@PathVariable int page, @PageableDefault (size = 5) Pageable pageable, Model model) {
-        Pageable requestedPage = PageRequest.of(page, pageable.getPageSize(), pageable.getSort());
-        Page<Study> studyPage = searchService.findAllWithPage(requestedPage);
-        model.addAttribute("studies", studyPage.getContent());
-        model.addAttribute("currentPage", page);
-        model.addAttribute("totalPages", studyPage.getTotalPages());
-        return "main"; // src/main/resources/templates/main.html 파일을 렌더링
+    @GetMapping("/patient")
+    @ResponseBody
+    public Patient getPatient(@RequestParam String pid) {
+        return searchService.getPatientByCode(Integer.parseInt(pid));
     }
 }
