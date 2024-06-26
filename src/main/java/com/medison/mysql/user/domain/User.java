9294d7676b17bf6f269074e35782894737a13f67
kimseungwoo449 <ehdanf449@naver.com>
2024-06-26 18:05:48 +0900

    add : 어딧팅

--- src/main/java/com/medison/mysql/user/domain/User.java
+++ src/main/java/com/medison/mysql/user/domain/User.java
@@ -1,16 +1,23 @@
 package com.medison.mysql.user.domain;
 
+import com.medison.mysql.user.dto.UserRequestDto;
+import com.medison.mysql.utill.Timestamped;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.sql.Timestamp;
 
+@NoArgsConstructor
 @Getter
 @Table(name = "user")
 @Entity
-public class User {
+public class User extends Timestamped {
     @Id
     private String id;
     private int departmentCode;
@@ -21,6 +28,20 @@ public class User {
     private String phone;
     private String birth;
     private String address;
-    private Timestamp regDate;
-    private Timestamp modDate;
+
+    public void create(UserRequestDto userRequestDto) {
+        this.id = userRequestDto.getId();
+        this.departmentCode = userRequestDto.getDepartmentCode();
+        this.password = userRequestDto.getPassword();
+        this.name = userRequestDto.getName();
+        this.position = userRequestDto.getPosition();
+        this.admin = userRequestDto.isAdmin();
+        this.phone = userRequestDto.getPhone();
+        this.birth = userRequestDto.getBirth();
+        this.address = userRequestDto.getAddress();
+    }
+
+    public void updateByTheUser(UserRequestDto userRequestDto) {
+        this.password = userRequestDto.getPassword();
+    }
 }
