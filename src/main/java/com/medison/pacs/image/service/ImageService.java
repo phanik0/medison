54cb13f6f689492bd9fff29a8aa1a1f36443b704
kimseungwoo449 <ehdanf449@naver.com>
2024-06-26 11:27:20 +0900

    add : studykey로 이미지 패스 불러오기 연습

--- src/main/java/com/medison/pacs/image/service/ImageService.java
+++ src/main/java/com/medison/pacs/image/service/ImageService.java
@@ -1,13 +1,18 @@
 package com.medison.pacs.image.service;
 
+import com.medison.pacs.image.domain.Image;
 import com.medison.pacs.image.domain.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ImageService {
     private final ImageRepository imageRepository;
 
-
+    public List<Image> findByStudykey(long studykey){
+        return imageRepository.findByStudykey(studykey);
+    };
 }
