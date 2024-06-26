cb42c42e4e6303c6a1607b8a8d88b98ad3c5d498
Lr2l1 <hoit5it@naver.com>
2024-06-26 18:00:09 +0900

    Modify 환자 변수명 변경 및 메인페이지와 연결 버그 수정

--- src/main/java/com/medison/mysql/patient/controller/PatientController.java
+++ src/main/java/com/medison/mysql/patient/controller/PatientController.java
@@ -3,24 +3,28 @@ package com.medison.mysql.patient.controller;
 import com.medison.mysql.patient.domain.Patient;
 import com.medison.mysql.patient.domain.PatientRepository;
 
+import com.medison.mysql.patient.service.PatientService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
 public class PatientController {
 
-    private final PatientRepository patientRepository;
+    private final PatientService patientService;
 
-//    @GetMapping("/patient")
-//    public ResponseEntity<Patient> getPatient(@RequestParam("pid") int pid) {
-//        Optional<Patient> patient = patientRepository.findById(pid);
-//        return patient.map(ResponseEntity::ok)
-//                .orElseGet(() -> ResponseEntity.notFound().build());
-//    }
+    @ResponseBody
+    @GetMapping("/patients")
+    public List<Patient> getAllPatients() {
+      List<Patient> result = patientService.findAll();
+      return result;
+ }
 }
