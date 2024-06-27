f8364d654dedccb4c39de6df5d87838e843e4c74
onceuponatim9 <sjung8018a@gmail.com>
2024-06-27 17:54:55 +0900

    Update Note.java

--- src/main/java/com/medison/mysql/note/domain/Note.java
+++ src/main/java/com/medison/mysql/note/domain/Note.java
@@ -1,48 +1,59 @@
 package com.medison.mysql.note.domain;
 
+import com.medison.mysql.user.domain.User;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Getter
 @Setter
-@Table(name = "NOTE")
+@Table(name = "note")
 @Entity
 public class Note {
 
     @Id
     private int code;
-    private long studykey;
+    private Long studykey;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "final_doctor", referencedColumnName = "id")
+//    private User finalDoctor;
+    @Column(name = "final_doctor")
     private String finalDoctor;
+
     private String patientCode;
     private String disease;
     private String treatmentPeriod;
     private String finding;
     private String comments;
     private String futureComment;
+
     @Column(name = "`usage`")
     private String usage;
+
     private Timestamp regDate;
     private Timestamp modDate;
 
-//    public void create(ReportRequestDto reportDto) {
-//        this.code = reportDto.getCode();
-//        this.studykey = reportDto.getStudykey();
-//        this.finalDoctor = reportDto.getFinalDoctor();
-//        this.patientCode = reportDto.getPatientCode();
-//        this.disease = reportDto.getDisease();
-//        this.treatmentPeriod = reportDto.getTreatmentPeriod();
-//        this.finding = reportDto.getFinding();
-//        this.comments = reportDto.getComments();
-//        this.futureComment = reportDto.getFutureComment();
-//        this.usage = reportDto.getUsage();
-//        this.regDate = reportDto.getRegDate();
-//        this.modDate = reportDto.getModDate();
+//    private String patientName;
+//    private String birth;
+//    private String history;
+//    private String caution;
+
+//    public void create(NoteRequestDto noteDto) {
+//        this.code = noteDto.getCode();
+//        this.studykey = noteDto.getStudykey();
+//        this.finalDoctor = noteDto.getFinalDoctor();
+//        this.patientCode = noteDto.getPatientCode();
+//        this.disease = noteDto.getDisease();
+//        this.treatmentPeriod = noteDto.getTreatmentPeriod();
+//        this.finding = noteDto.getFinding();
+//        this.comments = noteDto.getComments();
+//        this.futureComment = noteDto.getFutureComment();
+//        this.usage = noteDto.getUsage();
+//        this.regDate = noteDto.getRegDate();
+//        this.modDate = noteDto.getModDate();
 //    }
 
 }
