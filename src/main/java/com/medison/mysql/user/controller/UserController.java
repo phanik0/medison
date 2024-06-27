252c5f16369839c137f902f36556a50801103d0a
kimseungwoo449 <ehdanf449@naver.com>
2024-06-27 18:02:30 +0900

    modify : 맵핑값

--- src/main/java/com/medison/mysql/user/controller/UserController.java
+++ src/main/java/com/medison/mysql/user/controller/UserController.java
@@ -8,6 +8,7 @@ import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -16,11 +17,10 @@ import java.util.Map;
 
 @RequiredArgsConstructor
 @Controller
-@RequestMapping("/user")
 public class UserController {
     private final UserService userService;
     @ResponseBody
-    @PostMapping("/join")
+    @PostMapping("/user/join")
     public ModelAndView save(@ModelAttribute UserRequestDto userRequestDto) {
         System.out.println("de : " + userRequestDto.getDepartmentCode());
         ModelAndView modelAndView = new ModelAndView("");
@@ -41,25 +41,39 @@ public class UserController {
     }
 
     @ResponseBody
-    @GetMapping("/manage")
-    public List<UserResponseDto> getUsers() {
-        return userService.getUsers();
+    @GetMapping("/admin/manage")
+    public ModelAndView getUsers() {
+        ModelAndView modelAndView = new ModelAndView("userManage");
+        List<UserResponseDto> userResponseDtos = userService.getUsers();
+        modelAndView.addObject("infos", userResponseDtos);
+        return modelAndView;
     }
 
-    @ResponseBody
-    @PostMapping("/login")
-    public boolean login(@ModelAttribute UserRequestDto userRequestDto, HttpServletRequest request) {
+
+    @GetMapping("/user/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/user/login")
+    public RedirectView login(@ModelAttribute UserRequestDto userRequestDto, HttpServletRequest request) {
         HttpSession session = request.getSession();
         UserResponseDto result = userService.login(userRequestDto);
+        RedirectView redirectView = new RedirectView();
+
         if (result != null) {
             session.setAttribute("user", result);
-            return true;
-        } else
-            return false;
+            redirectView.setUrl("http://localhost:8080/admin");
+            return redirectView;
+        } else{
+            session.setAttribute("loginError", "loginError");
+            redirectView.setUrl("http://localhost:8080/user/login");
+            return redirectView;
+        }
     }
 
     @ResponseBody
-    @PostMapping("/logout")
+    @PostMapping("/user/logout")
     public boolean logout(HttpServletRequest request) {
         HttpSession session = request.getSession();
         session.removeAttribute("user");
@@ -71,13 +85,13 @@ public class UserController {
     }
 
     @ResponseBody
-    @DeleteMapping("/delete")
+    @DeleteMapping("/user/delete")
     public boolean delete(@ModelAttribute UserRequestDto userRequestDto) {
         return userService.delete(userRequestDto);
     }
 
     @ResponseBody
-    @PutMapping("/update/me")
+    @PutMapping("/user/update/me")
     public boolean updateByTheUser(@RequestParam Map<String, String> reqObj) {
         UserRequestDto userRequestDto = new UserRequestDto();
         userRequestDto.setId(reqObj.get("id").toString());
@@ -86,9 +100,15 @@ public class UserController {
     }
 
     @ResponseBody
-    @PutMapping("/update/admin")
+    @PutMapping("/user/update/admin")
     public boolean updateByTheAdmin(@ModelAttribute UserRequestDto userRequestDto) {
         return userService.updateByTheAdmin(userRequestDto);
+    }
+
+    // 어드민 페이지
+    @GetMapping("/admin")
+    public String admin() {
+        return "adminPage";
     }
 
 }
