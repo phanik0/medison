f73c5bc2a461f3f7e7cd87b21bb2e78a708b056f
Lr2l1 <hoit5it@naver.com>
2024-07-01 18:13:23 +0900

    Modify 메인페이지 북마크 삭제, 리스트 구현중

--- src/main/java/com/medison/mysql/bookmark/controller/BookmarkController.java
+++ src/main/java/com/medison/mysql/bookmark/controller/BookmarkController.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Controller
@@ -29,10 +31,39 @@ public class BookmarkController {
         }
     }
 
+    @PostMapping("/bookmark/delete")
+    @ResponseBody
+    public ResponseEntity<String> deleteBookmark(@RequestBody Bookmark bookmark) {
+        try {
+            bookmarkService.deleteBookmark(bookmark.getCode());
+            return ResponseEntity.ok("북마크가 삭제되었습니다.");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("북마크 삭제 중 오류가 발생했습니다: " + e.getMessage());
+        }
+    }
+
     @GetMapping("/bookmark/list")
     public String getBookmarkList(Model model, @RequestParam String userId) {
         List<Bookmark> bookmarks = bookmarkService.getBookmarksByUserId(userId);
+        List<Study> studies = bookmarkService.getStudiesForBookmarksByUserId(userId);
         model.addAttribute("bookmarks", bookmarks);
+        model.addAttribute("studies", studies);
         return "bookmark";
     }
+
+    @GetMapping("/bookmark/get")
+    public Map<String, Boolean> getBookmarks(@RequestParam String userId) {
+        try {
+            List<Bookmark> bookmarks = bookmarkService.getBookmarksByUserId(userId);
+            Map<String, Boolean> bookmarkMap = new HashMap<>();
+            for (Bookmark bookmark : bookmarks) {
+                bookmarkMap.put(String.valueOf(bookmark.getStudykey()), true);
+            }
+            return bookmarkMap;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("북마크 정보를 불러오는 중 오류가 발생했습니다.", e);
+        }
+    }
+
 }
